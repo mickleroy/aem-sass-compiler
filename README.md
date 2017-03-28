ff7f2425a667c7134c204484bcb306107d1e6f1f
Michael Leroy <mickjleroy@gmail.com>
2017-03-28 21:29:31 +1100

    created script compiler (just pushed sass file contents through for now)

---
+++ README.md
@@ -0,0 +1,18 @@
+
+### Install log
+```
+*INFO* [OsgiInstallerImpl] com.adobe.granite.ui.clientlibs.impl.CompilerProviderImpl Registering client library compiler scss
+*INFO* [OsgiInstallerImpl] com.github.mickleroy.aem-sass-compiler Service [com.github.mickleroy.aem.sass.impl.SassCompilerImpl,7730, [com.adobe.granite.ui.clientlibs.script.ScriptCompiler]] ServiceEvent REGISTERED
+```
+
+### Compilation log
+```
+*INFO* [0:0:0:0:0:0:0:1 [1490695427683] GET /etc/designs/aem-sass-compiler/clientlib.css HTTP/1.1] com.adobe.granite.ui.clientlibs.impl.HtmlLibraryManagerImpl Start building CSS library: /etc/designs/aem-sass-compiler/clientlib
+*INFO* [0:0:0:0:0:0:0:1 [1490695427683] GET /etc/designs/aem-sass-compiler/clientlib.css HTTP/1.1] com.github.mickleroy.aem.sass.impl.SassCompilerImpl Found source /etc/designs/aem-sass-compiler/clientlib/sample.scss
+*INFO* [0:0:0:0:0:0:0:1 [1490695427683] GET /etc/designs/aem-sass-compiler/clientlib.css HTTP/1.1] com.github.mickleroy.aem.sass.impl.SassCompilerImpl Compile Sass in 1ms
+*INFO* [0:0:0:0:0:0:0:1 [1490695427683] GET /etc/designs/aem-sass-compiler/clientlib.css HTTP/1.1] com.adobe.granite.ui.clientlibs.impl.HtmlLibraryManagerImpl finished building library /etc/designs/aem-sass-compiler/clientlib.css
+```
+
+### Client library cache clear
+[Link](http://localhost:4502/libs/granite/ui/content/dumplibs.rebuild.html?invalidate=true)
+
---
+++ bundle/src/main/java/com/github/mickleroy/aem/sass/impl/SassCompilerImpl.java
@@ -0,0 +1,80 @@
+package com.github.mickleroy.aem.sass.impl;
+
+import com.adobe.granite.ui.clientlibs.script.CompilerContext;
+import com.adobe.granite.ui.clientlibs.script.ScriptCompiler;
+import com.adobe.granite.ui.clientlibs.script.ScriptResource;
+import org.apache.commons.io.IOUtils;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Collection;
+
+
+@Component
+@Service(ScriptCompiler.class)
+public class SassCompilerImpl implements ScriptCompiler {
+
+    private static final String SCSS_EXTENSION = "scss";
+    private static final String CSS_EXTENSION = "css";
+    private static final String CSS_MIME_TYPE = "text/css";
+    private static final Logger log = LoggerFactory.getLogger(SassCompilerImpl.class);
+
+    public SassCompilerImpl() {
+        log.info("Creating instance of Sass Compiler");
+    }
+
+    @Override
+    public String getName() {
+        return SCSS_EXTENSION;
+    }
+
+    @Override
+    public boolean handles(String extension) {
+        return SCSS_EXTENSION.equals(extension) || ("." + SCSS_EXTENSION).equals(extension);
+    }
+
+    @Override
+    public String getMimeType() {
+        return CSS_MIME_TYPE;
+    }
+
+    @Override
+    public String getOutputExtension() {
+        return CSS_EXTENSION;
+    }
+
+    @Override
+    public void compile(Collection<ScriptResource> scriptResources, Writer dst, CompilerContext ctx) throws IOException {
+        long t0 = System.currentTimeMillis();
+
+        for(ScriptResource src : scriptResources) {
+            log.info("Found source {}", src.getName());
+            String scss = retrieveInputString(src);
+            log.debug("Scss source: {}", scss);
+
+            //TODO compile the shiz
+
+            dst.write(scss);
+        }
+
+        long t1 = System.currentTimeMillis();
+        log.info("Compiled Scss in {}ms", t1 - t0);
+    }
+
+    private static String retrieveInputString(ScriptResource resource) throws IOException {
+        String src = "";
+
+        try (Reader in = resource.getReader()) {
+            src = IOUtils.toString(in);
+            src = src.replace("\r", "");
+//            src = src.replace("@import-once", "@import (once)");
+        }
+
+        return src;
+    }
+}
